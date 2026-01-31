34eca360b8c40ca8bbc6d0b83ebba44e4b49efd0
dthomas16-1 <65570914+dthomas16-1@users.noreply.github.com>
2026-01-31 12:07:47 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = "grocery-app-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.webmanifest"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
